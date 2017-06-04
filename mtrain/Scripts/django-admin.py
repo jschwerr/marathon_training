7de41ce9c36024fa22df373f2d908a2060e3f5f9
Jacob Schwerr <schwerrj1228@gmail.com>
2017-06-04 08:20:42 -0400

    removed virtual environment

--- mtrain/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\schwe\desktop\forfun~1\python\marath~1\mtrain\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
