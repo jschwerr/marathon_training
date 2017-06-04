5e456a89c346ce68bdd27b511a8f80c3ebd72959
Jacob Schwerr <schwerrj1228@gmail.com>
2017-06-04 06:17:31 -0400

    initial commit

---
+++ mtrain/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\schwe\desktop\forfun~1\python\marath~1\mtrain\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
